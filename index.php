4f761fe2caa23c8d472174dd0b15f6b2b6ebc9c5
Dimitris Marlagkoutsos <info@sirtimid.com>
2017-12-04 16:13:21 +0200

    remove unwanted function

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<section <?php get_page_style() ?> >
+<section>
 	<article>
 		<h1><?php the_title(); ?></h1>
 		<div class="content"><?php the_content(); ?></div>
