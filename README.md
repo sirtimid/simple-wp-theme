cd5e07e2335bf783041297033bdf4da208ef965b
Dimitris Marlagkoutsos <info@sirtimid.com>
2019-07-13 14:26:43 +0300

    replaced grunt with gulp

--- README.md
+++ README.md
@@ -6,11 +6,10 @@ A very simple wordpress starter theme, intended for quick and simple websites.
 ### Requirements
 
 - [Node.js](http://nodejs.org/download/)
-- [Bower](http://bower.io/)
 
 ### Uses
 
-- [GruntJS](http://gruntjs.com/)
+- [Gulp](http://gulpjs.com)
 - [SASS](https://sass-lang.com)
 - [PostCSS](https://github.com/postcss/postcss)
 
@@ -20,23 +19,14 @@ Installation
 ```bash
 $ git clone https://github.com/sirtimid/simple-wp-theme.git
 $ cd ./simple-wp-theme
-$ npm install && bower install
+$ npm install
 ```
 
 Developing
 ------------
 
-Watch the change of *.scss, *.js files and build the .css
+Watch and build
 
 ```bash
-$ grunt watch
-```
-
-Build it
-------------
-
-Build the project to minify assets
-
-```bash
-$ grunt build
+$ gulp
 ```
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+/* gulp dependencies */
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
+const minifycss = require('gulp-minify-css')
+const concat = require('gulp-concat')
+const plumber = require('gulp-plumber')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const livereload = require('gulp-livereload')
+
+/* vendors task */
+gulp.task('vendors', function() {
+	return gulp.src([
+		'node_modules/fastclick/lib/fastclick.js',
+		'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+		'node_modules/flickity/dist/flickity.pkgd.js',
+		'node_modules/stickyfilljs/dist/stickyfill.min.js',
+	])
+	.pipe(concat('vendors.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('assets/js'))
+})
+
+/* scripts task */
+gulp.task('scripts', function() {
+	return gulp.src('assets/js/theme.js')
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('assets/js'))
+		.pipe(livereload())
+})
+
+/* sass task */
+gulp.task('sass', function() {
+	gulp.src('assets/scss/theme.scss')
+		.pipe(plumber())
+		.pipe(sass({
+			includePaths: ['scss']
+		}))
+		.pipe(gulp.dest('assets/css'))
+})
+
+/* postcss/cssnano task */
+gulp.task('css', function() {
+	var processors = [
+		autoprefixer(),
+		cssnano(),
+	]
+	return gulp.src('assets/css/theme.css')
+		.pipe(postcss(processors))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('assets/css'))
+		.pipe(livereload())
+})
+
+/* watch scss, js and html files, doing different things with each. */
+gulp.task('default', ['scripts', 'sass', 'css'], function() {
+	livereload.listen()
+	/* watch scss, run the sass task on change. */
+	gulp.watch(['assets/scss/**/*.scss'], ['sass', 'css'])
+	/* watch app.js file, run the scripts task on change. */
+	gulp.watch(['assets/js/theme.js'], ['scripts'])
+	/* watch .php and image files, run the reload task on change. */
+	gulp.watch(['**/*.php', '**/*.jpg', '**/*.png', '**/*.svg'], livereload.reload)
+})
